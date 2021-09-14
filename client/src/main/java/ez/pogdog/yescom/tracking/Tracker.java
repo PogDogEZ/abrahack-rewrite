60fd23efb00d89ac719caa6bbb15f9b02fd816e0
ARZI1 <69015828+ARZI1@users.noreply.github.com>
2021-09-14 16:52:05 +0300

    Improved Tracker class.

--- client/src/main/java/ez/pogdog/yescom/tracking/Tracker.java
+++ client/src/main/java/ez/pogdog/yescom/tracking/Tracker.java
@@ -1,11 +1,14 @@
 package ez.pogdog.yescom.tracking;
 
+import ez.pogdog.yescom.handlers.TrackingHandler;
 import ez.pogdog.yescom.tracking.algorithm.BasicAlgorithm;
 import ez.pogdog.yescom.tracking.algorithm.ITrackingAlgorithm;
 import ez.pogdog.yescom.tracking.information.TrackerPosition;
 import ez.pogdog.yescom.tracking.information.Trail;
 import ez.pogdog.yescom.util.ChunkPosition;
+import ez.pogdog.yescom.util.Dimension;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 /**
@@ -13,35 +16,65 @@ import java.util.UUID;
  */
 public class Tracker {
 
-    private final UUID uuid;
-
     private TrackerPosition position;
     private Trail trail;
 
-    private ITrackingAlgorithm algorithm;
+    private ITrackingAlgorithm currentAlgorithm;
+
+    private ArrayList<UUID> possiblePlayers = new ArrayList<>();
+
+    private boolean lost;
+    private int lostTicks;
 
     /**
      * Initialises the tracker with provided information.
      * @param loadedChunk The initial LoadedChunk which detected the player.
      */
-    public Tracker (ChunkPosition loadedChunk) {
-        uuid = UUID.randomUUID();
-
-        position = new TrackerPosition(loadedChunk);
+    public Tracker (ChunkPosition loadedChunk, Dimension dimension) {
+        position = new TrackerPosition(loadedChunk, dimension);
         trail = new Trail(loadedChunk);
 
-        algorithm = new BasicAlgorithm(getCenterPosition(loadedChunk));
+        currentAlgorithm = new BasicAlgorithm(this, getCenterPosition(loadedChunk, dimension));
+
+        lost = false;
+        lostTicks = 0;
     }
 
-    public Tracker (TrackedPlayer trackedPlayer) {
-        uuid = trackedPlayer.getMostLikelyPlayer();
-
+    public Tracker (TrackedPlayer trackedPlayer, Dimension dimension) {
         ChunkPosition logOutPos = trackedPlayer.getLogOutPos();
 
-        position = new TrackerPosition(logOutPos);
+        position = new TrackerPosition(logOutPos, dimension);
         trail = new Trail(logOutPos);
 
-        algorithm = new BasicAlgorithm(getCenterPosition(logOutPos));
+        currentAlgorithm = new BasicAlgorithm(this, getCenterPosition(logOutPos, dimension));
+
+        possiblePlayers.addAll(trackedPlayer.getPossiblePlayers());
+
+        lost = false;
+        lostTicks = 0;
+    }
+
+    public TrackingHandler.TrackerTickResult tick() {
+        ITrackingAlgorithm.TickResult tickResult = currentAlgorithm.onTick();
+
+        switch (tickResult) {
+            case Moved:
+                onMove(currentAlgorithm.getPlayerPosition(), currentAlgorithm.getDimension());
+            case Stagnant:
+                break;
+            case Lost:
+                lostTicks++;
+                break;
+        }
+
+        if (lostTicks > 0) {
+            if (!lost)
+                return TrackingHandler.TrackerTickResult.Found;
+
+            return TrackingHandler.TrackerTickResult.Lost;
+        }
+
+        return TrackingHandler.TrackerTickResult.Good;
     }
 
 
@@ -52,7 +85,24 @@ public class Tracker {
      * @param loadedChunk A chunk in the target's render distance.
      * @return The chunk in the middle of the target's render distance.
      */
-    private ChunkPosition getCenterPosition(ChunkPosition loadedChunk) {
+    private ChunkPosition getCenterPosition(ChunkPosition loadedChunk, Dimension dimension) {
         return new ChunkPosition(0, 0);
+    }
+
+    private void onMove(ChunkPosition chunkPosition, Dimension dimension) {
+        if (lost) {
+            onFound(chunkPosition, dimension);
+        }
+        position.moved(chunkPosition, dimension);
+    }
+
+    private void onFound(ChunkPosition chunkPosition, Dimension dimension) {
+        lost = false;
+    }
+
+    /* ------------------------ Public Methods ------------------------ */
+
+    public TrackerPosition getPosition() {
+        return position;
     }
 }
