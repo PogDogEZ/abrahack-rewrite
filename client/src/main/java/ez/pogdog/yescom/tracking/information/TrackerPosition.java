8d717887226efcebccf6ba6888f21c0cdfa327fe
ARZI1 <69015828+ARZI1@users.noreply.github.com>
2021-09-14 16:50:20 +0300

    Improved TrackerPosition.

--- client/src/main/java/ez/pogdog/yescom/tracking/information/TrackerPosition.java
+++ client/src/main/java/ez/pogdog/yescom/tracking/information/TrackerPosition.java
@@ -1,38 +1,56 @@
 package ez.pogdog.yescom.tracking.information;
 
-import ez.pogdog.yescom.util.BlockPosition;
 import ez.pogdog.yescom.util.ChunkPosition;
+import ez.pogdog.yescom.util.Dimension;
 
+
+/**
+ * Stores positional information used in the tracking process.
+ */
 public class TrackerPosition {
 
-    private final BlockPosition startingPosition;
+    private final ChunkPosition startingPosition;
 
-    private BlockPosition position;
+    private int posX;
+    private int posZ;
+    private Dimension dimension;
 
-    public TrackerPosition(BlockPosition startingPosition) {
+    public TrackerPosition(ChunkPosition startingPosition, Dimension dimension) {
         this.startingPosition = startingPosition;
-        position = startingPosition;
-    }
-
-    public TrackerPosition(ChunkPosition startingPosition) {
-        this(startingPosition.getPosition());
+        posX = startingPosition.getX();
+        posZ = startingPosition.getZ();
+        this.dimension = dimension;
     }
 
     /* ------------------------ Public Methods ------------------------ */
 
-    public BlockPosition getStartingPosition() {
+    public void moved(int newX, int newZ, Dimension newDimension) {
+        posX = newX;
+        posZ = newZ;
+        dimension = newDimension;
+    }
+
+    public void moved(ChunkPosition chunkPosition, Dimension dimension) {
+        moved(chunkPosition.getX(), chunkPosition.getZ(), dimension);
+    }
+
+    public ChunkPosition getStartingPosition() {
         return startingPosition;
     }
 
-    public BlockPosition getPosition() {
-        return position;
+    public ChunkPosition getPosition() {
+        return new ChunkPosition(posX, posZ);
+    }
+
+    public Dimension getDimension() {
+        return dimension;
     }
 
     public int getX() {
-        return position.getX();
+        return posX;
     }
 
     public int getZ() {
-        return position.getZ();
+        return posZ;
     }
 }
